04ba7c9c43cf0fa25e0b11aa8f6a1e1785afe87c
David Tunnell <davidtunnell9@gmail.com>
2021-08-24 11:44:13 -0500

    read functionality for each table being fetched and displayed on client app

--- client/helpers/apiConnection.js
+++ client/helpers/apiConnection.js
@@ -1,97 +1,38 @@
-const urlPrefix = "http://localhost:3001/";
+const fetch = require('node-fetch'); //https://www.npmjs.com/package/node-fetch
 
+const urlPrefix = "http://localhost:3001"; //make sure not to have a slash at the end
 
-const getDepartments = async() => {
-    const constructedUrl = urlPrefix + "/api/department/";
-    const resultObjects = [];
-    const response = await fetch(constructedUrl).then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-    }).then((data) => {
-        for (let i = 0; i < data.results.length; i++) {
-            var ele = data.results[i];
-            console.log(ele);
-            //rank doesn't always exist
-            // var rankTotal = 0;
-            // if (ele.community) {
-            //     rankTotal = ele.community.want + ele.community.have
-            // }
-            // var resultObject = {
-            //     title: ele.title,
-            //     image: ele.cover_image,
-            //     rank: rankTotal,
-            //     genre: ele.genre,
-            //     label: ele.label
-            // };
-        }
-    });
-    // return resultObjects;
+// const getDepartments = async() => {
+//     const constructedUrl = urlPrefix + "/api/department/";
+//     var resultObjects = [];
+//     await fetch(constructedUrl)
+//         .then(res => res.json())
+//         .then((json) => {
+//             resultObjects = json;
+//         });
+//     return resultObjects;
+// }
+
+function getAll(constructedUrl) {
+    return fetch(constructedUrl) // return this promise
+        .then(response => response.json())
+        .then(json => (json))
 }
 
-getDepartments();
+function getDepartments() {
+    const constructedUrl = urlPrefix + "/api/department/"
+    return getAll(constructedUrl);
+}
+
+function getRoles() {
+    const constructedUrl = urlPrefix + "/api/role/"
+    return getAll(constructedUrl);
+}
+
+function getEmployees() {
+    const constructedUrl = urlPrefix + "/api/employee/"
+    return getAll(constructedUrl);
+}
 
 
-
-// function getTrendingSingles(rankThreshold, year, genre, apiKey) {
-//     //create string to search by year
-//     var constructedUrl = "https://api.discogs.com/database/search?genre=" +
-//         genre + "&year=" +
-//         year + "&format=single&token=" + apiKey;
-//     //get the promise back and then once data comes back pass to update DOM
-//     var results = callDiscogsApi(constructedUrl, rankThreshold);
-//     results2
-//         for (let i = 0; i < data.results.length; i++) {
-//             var ele = data.results[i];
-//             //rank doesn't always exist
-//             var rankTotal = 0;
-//             if (ele.community) {
-//                 rankTotal = ele.community.want + ele.community.have
-//             }
-//             var resultObject = {
-//                 title: ele.title,
-//                 image: ele.cover_image,
-//                 rank: rankTotal,
-//                 genre: ele.genre,
-//                 label: ele.label
-//             };
-//             //filters out those that don't have sufficient rank calculated above
-//             if (resultObject.rank > rankThreshold) {
-//                 resultObjects.push(resultObject);
-//             }
-//         }
-//     });
-//     return resultObjects;
-// }
-
-// //call and get data from discogs API and return custom object array
-// async function callDiscogsApi(constructedUrl, rankThreshold) {
-//     var resultObjects = [];
-//     //get a response and then iterate through data, if it is ranked high enough add the object to the array
-//     const response = await fetch(constructedUrl).then(response => {
-//         if (response.ok) {
-//             return response.json();
-//         }
-//     }).then((data) => {
-//         for (let i = 0; i < data.results.length; i++) {
-//             var ele = data.results[i];
-//             //rank doesn't always exist
-//             var rankTotal = 0;
-//             if (ele.community) {
-//                 rankTotal = ele.community.want + ele.community.have
-//             }
-//             var resultObject = {
-//                 title: ele.title,
-//                 image: ele.cover_image,
-//                 rank: rankTotal,
-//                 genre: ele.genre,
-//                 label: ele.label
-//             };
-//             //filters out those that don't have sufficient rank calculated above
-//             if (resultObject.rank > rankThreshold) {
-//                 resultObjects.push(resultObject);
-//             }
-//         }
-//     });
-//     return resultObjects;
-// }
+module.exports = { getDepartments, getRoles, getEmployees };
--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const inquirerOptions = require('./helpers/inquirerOptions.js');
+const apiConnection = require('./helpers/apiConnection.js');
 
 const boldGreenConsoleFont = "\x1b[1m\x1b[32m";
 
@@ -14,8 +15,13 @@ const init = async() => {
         if (actionResponse.departmentMenu === "Create") {
             console.log(1);
         } else if (actionResponse.departmentMenu === "Read") {
-            console.log(2);
-            //TODO START HERE - use apiConnection.js
+            //get all departments using promise from fetch in the apiConnection file
+            apiConnection.getDepartments().then(departments => {
+                if (departments.wasSuccessful) {
+                    //using console.table for formatting
+                    console.table('\nDepartments', departments.body);
+                }
+            }).catch(err => console.log(err));
         } else if (actionResponse.departmentMenu === "Update") {
             console.log(3);
         } else if (actionResponse.departmentMenu === "Delete") {
@@ -26,25 +32,38 @@ const init = async() => {
         if (actionResponse.roleMenu === "Create") {
             console.log(1 + "r");
         } else if (actionResponse.roleMenu === "Read") {
-            console.log(2 + "r");
+            apiConnection.getRoles().then(roles => {
+                if (roles.wasSuccessful) {
+                    //using console.table for formatting
+                    console.table('\Roles', roles.body);
+                }
+            }).catch(err => console.log(err));
         } else if (actionResponse.roleMenu === "Update") {
             console.log(3 + "r");
         } else if (actionResponse.roleMenu === "Delete") {
             console.log(4 + "r");
         }
-    } else if (mainMenuResponse.mainMenu === "Employees") {
+    } else
+    if (mainMenuResponse.mainMenu === "Employees") {
         actionResponse = await inquirer.prompt(inquirerOptions.employeeMenu);
         if (actionResponse.employeeMenu === "Create") {
             console.log(1 + "e");
         } else if (actionResponse.employeeMenu === "Read") {
-            console.log(2 + "e");
+            apiConnection.getEmployees().then(employees => {
+                if (employees.wasSuccessful) {
+                    //using console.table for formatting
+                    console.table('\Roles', employees.body);
+                }
+            }).catch(err => console.log(err));
         } else if (actionResponse.employeeMenu === "Update") {
             console.log(3 + "e");
         } else if (actionResponse.employeeMenu === "Delete") {
             console.log(4 + "e");
         }
     }
-    wantToExit();
+    setTimeout(function() {
+        wantToExit();
+    }, 2000);
 };
 
 // https://stackoverflow.com/questions/51713333/how-to-terminate-npm-inquirer-prompt-and-return-control-to-main-menu-function
